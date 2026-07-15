fa6dd6bbb37f5d9cf567035ec0292dd7addb9b17
unknown <avishekshrabon3@gmail.com>
2026-07-15 09:50:01 +0600

    v1

---
+++ apps/website/app.js
@@ -0,0 +1,17 @@
+import { createServer } from "http";
+import next from "next";
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev, dir: __dirname });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> website ready on port ${port}`);
+  });
+});
